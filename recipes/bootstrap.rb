31987eadf9d054ce6c51669ef9499d526bacdc65
Jesse Nelson <spheromak@gmail.com>
2013-11-26 18:50:01 -0800

    fix tailor issue, and make sure we also catch ktc-monitor

--- recipes/bootstrap.rb
+++ recipes/bootstrap.rb
@@ -4,7 +4,8 @@
 # Author:: Jesse Nelson <spheromak@gmail.com>
 #
 
+match = /(ktc-)?monitor.*|(ktc-)?sensu.*|(ktc-)?graphite.*|(ktc-)?collectd.*/
 node[:base][:includes].each do |recipe|
-  next if recipe =~ /sensu.*|graphite.*|collectd.*|ktc-sensu.*|ktc-graphite.*|ktc-collectd.*/
+  next if recipe =~ match
   include_recipe recipe
 end
