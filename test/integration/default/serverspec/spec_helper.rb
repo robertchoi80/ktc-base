426803242ad2cde104a0759034fc03f2759c7beb
Jesse Nelson <spheromak@gmail.com>
2013-11-26 19:08:40 -0800

    remove unused bits of serverspec

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -7,14 +7,4 @@ include Serverspec::Helper::DetectOS
 
 RSpec.configure do |c|
   c.path = '/sbin:/usr/sbin'
-
-  if ENV['ASK_SUDO_PASSWORD']
-    require 'highline/import'
-    c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
-  else
-    c.sudo_password = ENV['SUDO_PASSWORD']
-  end
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
 end
