be4e558d13a657b30ad9162941a398eb4c288d27
Wil Reichert <wil.reichert@kt.com>
2013-12-09 14:30:09 +0900

    configure internal apt-repos

--- attributes/ubuntu.rb
+++ attributes/ubuntu.rb
@@ -1,6 +1,18 @@
 if platform == "ubuntu"
   include_attribute "ubuntu"
 
-  default[:ubuntu][:archive_url] = "http://ftp.daum.net/ubuntu"
-  default[:ubuntu][:security_url] = "http://ftp.daum.net/ubuntu"
+  case node.chef_environment
+  when "mkd2"
+    # mkds repo server
+    default[:ubuntu][:archive_url] = "http://repo02.mkd2.ktc/prod/ubuntu"
+    default[:ubuntu][:security_url] = "http://repo02.mkd2.ktc/prod/ubuntu"
+  when "ipc-stage", "ipc-ng"
+    # ipc repo server
+    default[:ubuntu][:archive_url] = "http://ngnode01.core.ucloud.com/prod/ubuntu"
+    default[:ubuntu][:security_url] = "http://ngnode01.core.ucloud.com/prod/ubuntu"
+  else
+    # external ip for ipc stage
+    default[:ubuntu][:archive_url] = "http://repo.kt-cloud.com/stage/ubuntu"
+    default[:ubuntu][:security_url] = "http://repo.kt-cloud.com/stage/ubuntu"
+  end
 end
