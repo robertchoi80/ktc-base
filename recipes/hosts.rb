ae8480ecb14d8fbb5c6f9de4b160de4cb13d15a4
Jesse Nelson <spheromak@gmail.com>
2013-12-10 21:57:09 -0800

    make the hostname entries explicit

--- recipes/hosts.rb
+++ recipes/hosts.rb
@@ -1,16 +1,29 @@
 
 hostsfile_entry '127.0.0.1' do
-  hostname  node['fqdn']
+  comment "Set by chef ktc-base::hosts"
+  hostname  'localhost.localdomain'
   ip_address '127.0.0.1'
   aliases   ['localhost']
-  action    :update
+  unique true
+  action [ :create, :update ]
 end
 
-ip = node[:ipaddress] || '127.0.0.1'
+# Ubuntu REQUIRES this to be setup corectly
+#   ipaddr  FQDN HOSTNAME
+#   ^^ has to be this order for hostname -f to work (and thus ohai)
+# the 127.0.1.1 entry shoudl be unused as it should only exist on hosts
+#  with no ipaddress
+ip = node[:ipaddress]
 hostsfile_entry ip do
+  not_if { ip == '127.0.0.1' }
+  action [ :create, :update ]
+  hostname node[:fqdn].empty? ? 'unknown.localdomain' : node[:fqdn]
+  aliases [ node[:hostname].empty? ? 'unknown' : node[:hostname] ]
   comment "Set by chef ktc-base::hosts"
-  hostname node[:hostname] || 'unknown'
   ip_address ip
-  aliases [node[:fqdn]] || 'unkown.localdomain'
-  unique false
+  unique true
+end
+
+hostsfile_entry '127.0.1.1' do
+  action :remove
 end
