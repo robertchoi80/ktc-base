65ee503ac64fa58cd71c8157980a86029a1829b4
Jesse Nelson <spheromak@gmail.com>
2013-12-10 20:46:20 -0800

    make sure localhost is set

--- recipes/hosts.rb
+++ recipes/hosts.rb
@@ -1,8 +1,16 @@
 
-hostsfile_entry node[:ipaddress] do
+hostsfile_entry '127.0.0.1' do
+  hostname  node['fqdn']
+  ip_address '127.0.0.1'
+  aliases   ['localhost']
+  action    :update
+end
+
+ip = node[:ipaddress] || '127.0.0.1'
+hostsfile_entry ip do
   comment "Set by chef ktc-base::hosts"
-  hostname node[:hostname]
-  ip_address node[:ipaddress]
-  aliases [node[:fqdn]]
-  unique true
+  hostname node[:hostname] || 'unknown'
+  ip_address ip
+  aliases [node[:fqdn]] || 'unkown.localdomain'
+  unique false
 end
