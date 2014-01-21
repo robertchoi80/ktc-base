8f3cae28ff4e211c7e718c844a07cce3228221f2
Jesse Nelson <spheromak@gmail.com>
2014-01-21 09:38:24 -0800

    Update logic to us management if it exists.

--- recipes/hosts.rb
+++ recipes/hosts.rb
@@ -15,14 +15,10 @@
 # 127.0.0.1 localhost.localdomain localhost
 #
 
-# On vagrant/dev env we use the eth1 ip addr explicitly
-# TODO: this is brittle There should be a better way to detect vagrant
-include_recipe "ktc-utils"
-if node.chef_environment == "dev"
-  ip = KTC::Network.address "management"
-else
-  ip = node[:ipaddress]
-end
+# use the management interface if it exists as the ipaddress.
+# This has some side-effects in chef. The node.fqdn may change.
+# after this then things will work.
+ip = KTC::Network.address "management" ||  node[:ipaddress]
 
 # we don't want to manage or force this on a host with only loopback
 return if ip == '127.0.0.1'
