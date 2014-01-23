e61503c5f33cfe39540e3b8667b9ccc447364fc6
Wil Reichert <wil.reichert@kt.com>
2014-01-23 11:57:26 +0900

    dont force chef-client download

--- attributes/omnibus_updater.rb
+++ attributes/omnibus_updater.rb
@@ -10,3 +10,5 @@ default[:omnibus_updater][:version] = "latest"
 # see https://github.com/hw-cookbooks/omnibus_updater/issues/20
 default[:omnibus_updater][:kill_chef_on_upgrade]   = false
 default[:omnibus_updater][:remove_chef_system_gem] =  true
+default[:omnibus_updater][:always_download] = false
+default[:omnibus_updater][:cache_omnibus_installer] = true
