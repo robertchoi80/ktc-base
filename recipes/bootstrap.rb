9f511875e254f9388ca661355834970f9c8414b2
Robert <taeil.choi@kt.com>
2014-03-06 14:15:53 +0900

    add 'ktc-logging' to exclude pattern on bootstrap

--- recipes/bootstrap.rb
+++ recipes/bootstrap.rb
@@ -4,7 +4,7 @@
 # Author:: Jesse Nelson <spheromak@gmail.com>
 #
 
-match = /(ktc-)?monitor.*|(ktc-)?sensu.*|(ktc-)?graphite.*|(ktc-)?collectd.*/
+match = /(ktc-)?monitor.*|(ktc-)?sensu.*|(ktc-)?graphite.*|(ktc-)?collectd.*|(ktc-)?logging.*/
 node[:base][:includes].each do |recipe|
   next if recipe =~ match
   include_recipe recipe
