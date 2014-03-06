c5a78d2073aa6f4071aca2a4dc66d6be48b597bd
Jesse Nelson <spheromak@gmail.com>
2014-03-05 22:13:17 -0800

    break up regex into multi-line

--- recipes/bootstrap.rb
+++ recipes/bootstrap.rb
@@ -4,7 +4,14 @@
 # Author:: Jesse Nelson <spheromak@gmail.com>
 #
 
-match = /(ktc-)?monitor.*|(ktc-)?sensu.*|(ktc-)?graphite.*|(ktc-)?collectd.*|(ktc-)?logging.*/
+match = /
+  (ktc-)?monitor.*|
+  (ktc-)?sensu.*|
+  (ktc-)?graphite.*|
+  (ktc-)?collectd.*|
+  (ktc-)?logging.*
+/
+
 node[:base][:includes].each do |recipe|
   next if recipe =~ match
   include_recipe recipe
